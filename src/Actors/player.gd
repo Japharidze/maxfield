49666c88548c1d1ef4de2f027eabeb16ee08c9f6
Japharidze <sergi.japharidze@gmail.com>
2023-08-14 09:36:39 +0400

    animation tries using Animation Tree draft

--- src/Actors/player.gd
+++ src/Actors/player.gd
@@ -6,8 +6,12 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var character_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var character_sprite : Sprite2D = $Sprite2D
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
 
+func _ready():
+	anim_state.travel("moving")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,8 +25,10 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	print(direction)
 	if direction:
-		character_sprite.flip_h = not bool(direction+1)
+		character_sprite.flip_h = bool(direction+1)
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
